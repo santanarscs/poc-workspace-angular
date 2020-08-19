e62f4adf83a9804254f712fcd9554ec5cf522ffa
santanarscs <raphaelstn@gmail.com>
2020-08-18 21:28:20 -0300

    feat: add library dynamic-forms

---
+++ projects/dynamic-forms/src/lib/dynamic-forms.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-dynamic-forms',
+  template: `
+    <p>
+      dynamic-forms works!
+    </p>
+  `,
+  styles: [],
+})
+export class DynamicFormsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ projects/dynamic-forms/src/lib/dynamic-forms.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { DynamicFormsComponent } from './dynamic-forms.component';
+
+@NgModule({
+  declarations: [DynamicFormsComponent],
+  imports: [],
+  exports: [DynamicFormsComponent],
+})
+export class DynamicFormsModule {}
