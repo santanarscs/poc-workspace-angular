780044d4f3bbcf21eb4118810f0a560ee5a45a71
santanarscs <raphaelstn@gmail.com>
2020-08-20 11:57:39 -0300

    feat: remove lib deprecated

--- projects/dynamic-forms/src/lib/dynamic-forms.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { DynamicFormsComponent } from './dynamic-forms.component';
-
-@NgModule({
-  declarations: [DynamicFormsComponent],
-  imports: [],
-  exports: [DynamicFormsComponent],
-})
-export class DynamicFormsModule {}
