0ae51dc72fdc0bc96c94dafbb66e870243108299
santanarscs <raphaelstn@gmail.com>
2020-08-21 18:23:06 -0300

    feat: import dynamic forms in flowable library

--- projects/flowable/src/lib/flowable.module.ts
+++ projects/flowable/src/lib/flowable.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskComponent } from './components/task/task.component';
 import { HttpClientModule } from '@angular/common/http';
 
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 
+import { DynamicFormModule } from 'dynamic-form';
+
 @NgModule({
-  declarations: [TaskListComponent],
-  imports: [HttpClientModule, BrowserModule, MatListModule, MatIconModule],
+  declarations: [TaskListComponent, TaskComponent],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    MatListModule,
+    MatIconModule,
+    DynamicFormModule.forRoot(),
+  ],
   exports: [TaskListComponent],
 })
 export class FlowableModule {}
